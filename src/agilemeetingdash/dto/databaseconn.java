d6995159fad2c81e7387a7cf091cb7e20adb558c
Jay Chiddarwar <jschiddarwar@gmail.com>
2023-10-20 16:03:13 -0700

    update gitignore and changed cred

--- src/agilemeetingdash/dto/databaseconn.java
+++ src/agilemeetingdash/dto/databaseconn.java
@@ -20,18 +20,6 @@ public class databaseconn {
     	
     	if(created == 0 ) {
     		
-//    		// postgre from here
-//    		String dbDriver = "org.postgresql.Driver"; 
-//	        String dbURL = "jdbc:postgresql://ec2-54-246-85-151.eu-west-1.compute.amazonaws.com:5432/d1u6s4u371k8e3"; 
-//	          // Database name to access 
-//	        String dbName = "agilemeeting?serverTimezone=UTC"; 
-//	        String dbUsername = "gkgiokyhmaxznd"; 
-//	        String dbPassword = "ff8ff1a1bfbe7b5ed5ec67483250fc66fd0b41cf6e500b545b6b73108ee3d5bc"; 
-//	  
-//	        Class.forName(dbDriver); 
-//	        Connection con = DriverManager.getConnection(dbURL, dbUsername, dbPassword ); 
-////	        end here
-    		
 	        // mysql from here
     		String dbDriver = "com.mysql.jdbc.Driver"; 
             String dbURL = "jdbc:mysql://localhost:3306/"; 
